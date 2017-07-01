62c6dc8dbabd5fe1e20b2eae7b3039ed9bea1fc1
Nick <komarnickolas@gmail.com>
2017-07-01 09:17:59 -0600

    Logged in shown

--- main.rb
+++ main.rb
@@ -46,8 +46,8 @@ end
 
 post '/sign_in' do
   user = User.find_by(username: params[:username])
-  if user && user.authenticate(params[:password_digest])
-    session[:user_id] = user.id
+  if user && user.authenticate(params[:password])
+    session[:user] = user
   end
   redirect '/'
 end
