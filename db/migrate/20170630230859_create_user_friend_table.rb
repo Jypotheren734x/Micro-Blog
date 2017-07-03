f8ed1735f8a06e126588938684efa0f5996ca432
Nick <komarnickolas@gmail.com>
2017-07-03 11:08:16 -0600

    Updates

--- db/migrate/20170630230859_create_user_friend_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateUserFriendTable < ActiveRecord::Migration
-  def change
-    create_table(:user_friends) do |t|
-      t.integer :user_id
-      t.integer :friend_id
-    end
-  end
-end
